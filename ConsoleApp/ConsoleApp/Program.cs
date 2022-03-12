4f30f80660918326cad76c733c8b8939bbef73a9
Ibrahim Rizq <ibrahimrizqabd@gmai.com>
2022-03-12 12:15:56 +0200

    first task

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.IO;
+
 
 namespace ConsoleApp
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = Directory.GetCurrentDirectory();
+            String command="";
+            String commandText;
+            String arg ="";
+            int i = 0;
+            while (true)
+            {
+                command = "";
+                Console.Write(path + ">");
+                commandText = Console.ReadLine();
+                string[] separatingStrings = { " -", };
+                
+                string[] words = commandText.Split(separatingStrings, System.StringSplitOptions.RemoveEmptyEntries);
+                
+                if (commandText != "")
+                    command = words[0];
+                /* foreach (char ch in commandText) {
+                    if (ch == '-')
+                        arg += commandText[i + 1];
+                    else if (ch != ' ')
+                        command += ch;
+                    i++;
+                }*/
+
+
+                if (commandText == "")
+                    continue;
+                else if (command == "exit")
+                    Environment.Exit(0);
+                else if (command == "clear")
+                    Console.Clear();
+                else if (command == "help")
+                    Console.WriteLine(File.ReadAllText(@"F:\college\third_year\second semester\compilers\project\help.txt"));
+                else Console.Write("'" + commandText + "'" + "is not recognized as an internal or external command,\n operable program or batch file\n");
+
+
+            }
         }
     }
 }
